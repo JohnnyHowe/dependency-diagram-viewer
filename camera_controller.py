b0e3f4811db1c765717f9c758c2ffc5a670cbb72
Johnny Howe <jonathon.o.howe@gmail.com>
2025-11-16 18:40:03 +1300

    camera controller mouse down and up events

--- camera_controller.py
+++ camera_controller.py
@@ -10,7 +10,24 @@ class CameraController:
     def __init__(self) -> None:
         if not pygame.get_init():
             pygame.init()
+        self._last_pressed_state = False
+        self._last_mouse_position = Vector2(0, 0)
 
     def update(self):
-        self._last_mouse_position = Vector2(pygame.mouse.get_pos())
-        self._last_pressed_state = pygame.mouse.get_pressed()[self.button_number]
+        new_position = Vector2(pygame.mouse.get_pos())
+        new_pressed_state = pygame.mouse.get_pressed()[self.button_number]
+
+        if new_pressed_state and not self._last_pressed_state:
+            self._mouse_down()
+        elif not new_pressed_state and self._last_pressed_state:
+            self._mouse_up()
+
+        self._last_mouse_position = new_position 
+        self._last_pressed_state = new_pressed_state
+
+    def _mouse_up(self):
+        print("mouse up")
+
+    def _mouse_down(self):
+        print("mouse down")
+
