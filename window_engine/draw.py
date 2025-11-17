6a46997aff4fea8358556cb1044bd66798b8b66f
Johnny Howe <jonathon.o.howe@gmail.com>
2025-11-17 18:44:40 +1300

    refactored text drawing code

--- window_engine/draw.py
+++ window_engine/draw.py
@@ -15,21 +15,32 @@ def text(text: str, size: float, containing_rect: Rect, color="#ffffff"):
     text_screen_space(text, int(Camera().project_size_component(size)), Camera().project_rect(containing_rect), color)
 
 
-def text_screen_space(text: str, size: float, containing_rect: Rect, color="#ffffff"):
-    lines = text.splitlines()
-    font = FontCache().get_font(int(size))
+def text_screen_space(text: str, size: int, containing_rect: Rect, color="#ffffff"):
+    max_font_size = _get_max_font_size_for_text_to_fit(text, Vector2(containing_rect.size))
+    font_size = min(max_font_size, size)
+    font = FontCache().get_font(font_size)
+    
+    current_position = Vector2(containing_rect.topleft)
+    for line in text.splitlines():
+        Window().surface.blit(font.render(line, True, color), current_position)
+        current_position.y += font_size
 
-    real_space_used_with_target_font_size = Vector2(0, 0)
-    for line in lines:
-        real_space_used_with_target_font_size += Vector2(font.size(line))
 
-    scale_factor = min(containing_rect.width / real_space_used_with_target_font_size.x, containing_rect.height / real_space_used_with_target_font_size.y)
+def _get_max_font_size_for_text_to_fit(text: str, area: Vector2) -> int:
+    n_lines = len(text.splitlines())
+    start_size_guess = area.y / n_lines
+
+    space_used =_get_space_rendered_text_will_use(text, start_size_guess) 
+    scale_factor = min(area.x / space_used.x, area.y / space_used.y)
     scale_factor = min(scale_factor, 1)
 
-    real_font_size = math.floor(size * scale_factor)
-    font = FontCache().get_font(int(real_font_size))
-    
-    position = Vector2(containing_rect.topleft)
-    for line in lines:
-        Window().surface.blit(font.render(line, True, color), position)
-        position.y += real_font_size
+    real_font_size = math.floor(start_size_guess * scale_factor)
+    return real_font_size
+ 
+
+def _get_space_rendered_text_will_use(text: str, screen_font_size: int) -> Vector2:
+    font = FontCache().get_font(int(screen_font_size))
+    real_space_used_with_target_font_size = Vector2(0, 0)
+    for line in text.splitlines():
+        real_space_used_with_target_font_size += Vector2(font.size(line))
+    return real_space_used_with_target_font_size
