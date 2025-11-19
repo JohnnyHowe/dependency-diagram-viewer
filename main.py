175bc661d2af539537bbcae11f545b709e84b2fd
Johnny Howe <jonathon.o.howe@gmail.com>
2025-11-20 11:51:13 +1300

    run from command line

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import argparse
+import os
 from diagram_viewer import DiagramViewer
 
-DiagramViewer("./dwarfhold.json")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    args = parser.parse_args()
+
+    if not os.path.exists(args.path):
+        print(f"File \"{args.path}\" does not exist")
+        return
+
+    DiagramViewer(args.path)
+
+
+if __name__ == "__main__":
+    main()
