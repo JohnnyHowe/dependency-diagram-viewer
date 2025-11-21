ef756e8eb05e412152c264ff59ae9c8556d3e89c
Johnny Howe <jonathon.o.howe@gmail.com>
2025-11-22 09:56:21 +1300

    DiagramViewer can view script to module dependencies

--- diagram/diagram_loader.py
+++ diagram/diagram_loader.py
@@ -8,7 +8,7 @@ from diagram.diagram_script import DiagramScript
 class DiagramLoader:
     def __init__(self, file_path: str):
         self.file_path = file_path
-        self._all_scripts = {}
+        self._all_items = {}
         self._load()
 
     def _load(self):
@@ -24,6 +24,7 @@ class DiagramLoader:
         module = DiagramModule(data["path"], data["name"], parent)
         module.is_hidden = data.get("is_hidden", False)
         module.is_collapsed = data.get("is_collapsed", False)
+        self._all_items[module.path] = [module, data]
 
         for script_data in data["scripts"]:
             module.scripts.append(self._create_script_from_data_dict(module, script_data))
@@ -36,18 +37,19 @@ class DiagramLoader:
     def _create_script_from_data_dict(self, parent, data: dict) -> DiagramScript:
         script = DiagramScript(data["full_name"], data["name"], data["path"], parent, data.get("position", Vector2(0, 0)))
         script.is_hidden = data.get("is_hidden", False)
-        self._all_scripts[script.full_name] = [script, data]
+        self._all_items[script.full_name] = [script, data]
         return script
 
     def _populate_all_script_dependencies(self):
-        for script, json_data in self._all_scripts.values():
-            self._populate_script_dependencies(script, json_data)
+        for script, json_data in self._all_items.values():
+            if isinstance(script, DiagramScript):
+                self._populate_script_dependencies(script, json_data)
 
     def _populate_script_dependencies(self, script: DiagramScript, json_data: dict):
         script.dependencies = []
         dependency_names = json_data["dependencies"]
         for dependency_full_name in dependency_names:
-            script.dependencies.append(self._all_scripts[dependency_full_name][0])
+            script.dependencies.append(self._all_items[dependency_full_name][0])
 
     def get_root(self):
         return self._root
